cc620c294befb5143560d31bd2853f8d85ee456e
Gabeeh94 <62768516+Gabeeh94@users.noreply.github.com>
2022-02-03 12:59:07 -0300

    Update Sunshine_normalization.py

--- Scripts/Sunshine_normalization.py
+++ Scripts/Sunshine_normalization.py
@@ -57,36 +57,6 @@ for i in range (0, len(countries)):
     
     countries_mean = countries_mean.append(row, ignore_index=True)
     
-#Create Dataframe to put the sunlight level of countries
-
-countries_sunlight_level = pd.DataFrame(columns = ['Country','Sunlight Level'])
-
-var = 'MeanAnnualSunlightHours'
-
-#Define Quartiles to dscriminate fairly how the countries should be categorized
-
-Q1 = np.percentile(countries_mean[var], 25, interpolation = 'midpoint')
-Q2 = np.percentile(countries_mean[var], 50, interpolation = 'midpoint')
-Q3 = np.percentile(countries_mean[var], 75, interpolation = 'midpoint')
-
-# Get the countries categorized
-
-low = countries_mean.loc[(countries_mean[var] <= Q1)].assign(level = 'Low')
-medium_low = countries_mean.loc[(countries_mean[var]> Q1) & (countries_mean[var] <= Q2)].assign(level = 'Medium-Low')
-medium_high = countries_mean.loc[(countries_mean[var]> Q2) & (countries_mean[var] <= Q3)].assign(level = 'Medium-High')
-high = countries_mean.loc[(countries_mean[var] > Q3)].assign(level = 'High')
-
-#Append those countries to a dataframe with the countries by sunlight level
-
-for i in (low, medium_low, medium_high, high):
-    for j in range (0, len(i)):
-        country = i.iloc[j,0]
-
-        level = i.iloc[j,2]
-        
-        row = pd.Series([country,level], index=countries_sunlight_level.columns)
-        
-        countries_sunlight_level = countries_sunlight_level.append(row, ignore_index=True)
 
 #Set the environment to upload the dataframe to a SQL Server table
 
